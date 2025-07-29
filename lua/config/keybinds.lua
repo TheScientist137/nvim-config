b998aa1cf7da9fa6a6a1abbe33f355c731b9ef4b
sargonass <thescientist137@outlook.es>
2025-07-29 17:53:07 +0200

    added plugins conform nvim-lint with basic config

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -5,9 +5,9 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>d", "<cmd>Neotree toggle reveal left<cr>") -- Open tree and focus on current file
 
 -- Toggleterm
-local term = '<Cmd>ToggleTerm direction=horizontal size=15<CR>'
+local term = "<Cmd>ToggleTerm direction=horizontal size=15<CR>"
 
 -- Normal → abre la terminal
-vim.keymap.set('n', '<C-t>', term, { desc = 'Terminal toggle' })
+vim.keymap.set("n", "<C-t>", term, { desc = "Terminal toggle" })
 -- Dentro de la terminal → mismo atajo la cierra
-vim.keymap.set('t', '<C-t>', term, { desc = 'Terminal toggle' })
+vim.keymap.set("t", "<C-t>", term, { desc = "Terminal toggle" })
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 3000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>l", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
