99a29ccc8ba24bc12a16d51dd556a8bc27b7b900
sargonass <thescientist137@outlook.es>
2025-08-04 20:07:42 +0200

    added auto-pairs plugin

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,12 +2,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 -- Neotree
-vim.keymap.set("n", "<leader>d", "<cmd>Neotree toggle reveal left<cr>") -- Open tree and focus on current file
+vim.keymap.set("n", "<leader>d", "<cmd>Neotree toggle reveal<cr>") -- Open tree and focus on current filee
 
 -- Toggleterm
 local term = "<Cmd>ToggleTerm direction=horizontal size=15<CR>"
 
--- Normal → abre la terminal
-vim.keymap.set("n", "<C-t>", term, { desc = "Terminal toggle" })
--- Dentro de la terminal → mismo atajo la cierra
-vim.keymap.set("t", "<C-t>", term, { desc = "Terminal toggle" })
+vim.keymap.set("n", "<C-t>", term, { desc = "Terminal toggle" }) -- Open/Close terminal in normal mode
+vim.keymap.set("t", "<C-t>", term, { desc = "Terminal toggle" }) -- Open/Close terminal in terminal mode
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,21 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
+
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+vim.opt.mouse = "a"
+
+vim.g.editorconfig = true
