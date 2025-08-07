a11da00e397ff8e77a918fcf8082ee69898b3eb2
TheScientist137 <thescientist137@outlook.es>
2025-08-07 18:31:55 +0200

    added lsp-nvim configs for languages

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,26 @@
+return {
+    -- Command and arguments to start the server
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = {
+        ".luarc.json",
+        ".luarc.jsonc",
+        ".luacheckrc",
+        ".stylua.toml",
+        "stylua.toml",
+        "selene.toml",
+        "selene.yml",
+        ".git",
+    },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the 'vim' global
+                globals = { "vim" },
+            },
+        },
+    },
+}
